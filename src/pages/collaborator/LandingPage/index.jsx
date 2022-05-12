bc173ea494fa7ad7391994a32ef8d3216cf87817
Kevin <kevinaryel.dev@gmail.com>
2022-05-12 15:44:36 -0300

    feat(landing-page):first version created

--- src/pages/collaborator/LandingPage/index.jsx
+++ src/pages/collaborator/LandingPage/index.jsx
@@ -1,8 +1,145 @@
+import {
+  LeftContainer,
+  NavbarContainer,
+  RightContainer,
+  NavbarButton,
+  ModalLinksButton,
+  MainDescription,
+  SectionTitle,
+  SectionLanding,
+  SectionAboutUs,
+  TopicAboutUs,
+  SectionTestimony,
+  ModalNav,
+} from "./../../../styles/landing-page.js";
+
+import TabletAboutUs from "./../../../img/Tablet-AboutUs.png";
+import TabletLandingPage from "./../../../img/Tablet-LandingPage.png";
+import DinnerAboutUs from "./../../../img/Dinner-AboutUs.png";
+import DashboardAboutUs from "./../../../img/Dashboard-AboutUs.png";
+import { FaBars } from "react-icons/fa";
+
+import { AiOutlineClose } from "react-icons/ai";
+
+import { useState } from "react";
+
 const LandingPage = () => {
-    return(
-        <div>
-            LandingPage
-        </div>
-    )
-}
-export default LandingPage
+  const [openLinks, setOpenLinks] = useState(false);
+  return (
+    <div>
+      <NavbarContainer>
+        <LeftContainer>
+          <ModalLinksButton
+            onClick={() => {
+              setOpenLinks((curr) => !curr);
+            }}
+          >
+            {openLinks ? (
+              <AiOutlineClose />
+            ) : (
+              <>
+                <FaBars />
+              </>
+            )}
+          </ModalLinksButton>
+          <h2>Cardápio Digital</h2>
+        </LeftContainer>
+        <RightContainer>
+          <NavbarButton>Login</NavbarButton>
+          <NavbarButton>Cadastre-se</NavbarButton>
+        </RightContainer>
+      </NavbarContainer>
+      {openLinks && (
+        <ModalNav>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#aboutUs">Sobre Nós</a>
+            </li>
+            <li>
+              <a href="#testimony">Depoimentos</a>
+            </li>
+          </ul>
+        </ModalNav>
+      )}
+
+      <SectionLanding id="home">
+        <SectionTitle>Aplicativo de Cardápio Digital</SectionTitle>
+        <MainDescription>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500. Modernize seu estabelecimento alimentício com a
+          aplicação do futuro!
+        </MainDescription>
+        <img src={TabletLandingPage} alt="" />
+      </SectionLanding>
+
+      <SectionAboutUs id="aboutUs">
+        <SectionTitle>Sobre nós</SectionTitle>
+        <ul>
+          <TopicAboutUs>
+            <div>
+              <div className="decoration"></div>
+              <img src={TabletAboutUs} alt="Nossa Aplicação" />
+            </div>
+            <section>
+              <h2>Sustentável</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </section>
+          </TopicAboutUs>
+          <TopicAboutUs>
+            <section>
+              <h2>Maior privacidade</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </section>
+            <div>
+              <img src={DinnerAboutUs} alt="Exemplo Mesa de Restaurante" />
+              <div className="decoration"></div>
+            </div>
+          </TopicAboutUs>
+          <TopicAboutUs>
+            <div>
+              <img src={DashboardAboutUs} alt="Nossa Aplicação - Dashboard" />
+              <div className="decoration"></div>
+            </div>
+            <section>
+              <h2>Controle total do restaurante</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </section>
+          </TopicAboutUs>
+        </ul>
+      </SectionAboutUs>
+
+      <SectionTestimony id="testimony">
+        <SectionTitle>Depoimentos</SectionTitle>
+      </SectionTestimony>
+    </div>
+  );
+};
+export default LandingPage;
---
+++ src/styles/landing-page.js
@@ -0,0 +1,226 @@
+import styled from "styled-components";
+
+export const NavbarContainer = styled.nav`
+  width: 100%;
+  height: fit-content;
+  background-color: var(--secondary-color);
+  display: flex;
+  height: 10vh;
+  min-width: 320px;
+  position: fixed;
+`;
+
+export const LeftContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 5%;
+  gap: 28px;
+  @media (min-width: 1024px) {
+    padding-left: 10%;
+  }
+
+  h2 {
+    font-style: black;
+    font-size: 36px;
+    line-height: 49px;
+    background: -webkit-linear-gradient(#485d5e, var(--primary-color));
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    display: none;
+    font-family: "Noto Serif", serif;
+
+    @media (min-width: 700px) {
+      display: block;
+    }
+  }
+`;
+
+export const RightContainer = styled.div`
+  flex: auto;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding-right: 20px;
+  gap: 10px;
+`;
+
+export const NavbarButton = styled.button`
+  border: none;
+  background-color: inherit;
+  color: var(--primary-color);
+  padding: 10px;
+  font-size: 24px;
+  height: fit-content;
+  min-width: fit-content;
+  margin: 10px 0;
+  font-family: "Noto Serif", serif;
+  cursor: pointer;
+  :hover {
+    border-radius: 4px;
+    background-color: var(--secondary-color-50);
+  }
+`;
+
+export const ModalLinksButton = styled.button`
+  background: none;
+  border: none;
+  color: var(--primary-color);
+  font-size: 36px;
+  cursor: pointer;
+  margin-bottom: -7px;
+`;
+
+export const ModalNav = styled.div`
+  width: fit-content;
+  min-width: 320px;
+  height: fit-content;
+  background-color: var(--secondary-color);
+  position: fixed;
+  border-radius: 0 0 10px 0px;
+  top: 10vh;
+
+  ul {
+    list-style: none;
+    display: flex;
+    gap: 10px;
+    flex-direction: column;
+    align-items: center;
+
+    li {
+      padding: 10px 0;
+      width: 80%;
+      border-bottom: 2px solid var(--primary-color);
+      box-sizing: border-box;
+      text-align: center;
+    }
+    li:last-child {
+      border: none;
+    }
+    a {
+      color: var(--primary-color);
+      font-family: "Noto Sans", sans-serif;
+      font-size: 15px;
+    }
+  }
+`;
+
+export const SectionLanding = styled.section`
+  min-height: 100vh;
+  padding-top: 10vh;
+  height: fit-content;
+  background-color: var(--primary-color);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-width: 320px;
+
+  img {
+    width: 35%;
+    height: auto;
+  }
+`;
+export const SectionTitle = styled.h1`
+  font-weight: 700;
+  font-size: 64px;
+  line-height: 77px;
+  color: var(--secondary-color);
+  text-align: center;
+  margin-bottom: 15px;
+  font-family: "Noto Sans", sans-serif;
+  margin-top: 50px;
+`;
+
+export const MainDescription = styled.h3`
+  font-weight: 400;
+  width: 60vw;
+  font-size: 24px;
+  line-height: 29px;
+  text-align: center;
+  color: white;
+  margin-bottom: 15px;
+  margin-top: 0px;
+  font-family: "Inter", sans-serif;
+`;
+export const SectionAboutUs = styled.section`
+  min-width: 320px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--primary-color);
+
+  ul {
+    list-style: none;
+    width: 90%;
+    min-width: 320px;
+    margin-top: 50px;
+    gap: 70px;
+    display: flex;
+    flex-direction: column;
+  }
+  h1 {
+    font-size: 64px;
+    font-weight: 700;
+    line-height: 77px;
+    color: var(--secondary-color);
+  }
+`;
+
+export const TopicAboutUs = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: stretch;
+  align-items: center;
+  gap: 40px;
+  height: fit-content;
+  div {
+    width: 50%;
+    height: auto;
+    img {
+      width: 100%;
+      height: auto;
+      border-radius: 20px;
+    }
+    .decoration {
+    }
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    text-align: center;
+    justify-content: space-between;
+    height: inherit;
+
+    h2 {
+      font-size: 29px;
+      font-weight: 700;
+      line-height: 40px;
+      letter-spacing: 0em;
+      text-align: center;
+      color: var(--secondary-color);
+      font-family: "Noto Sans", sans-serif;
+      justify-self: flex-start;
+    }
+    p {
+      font-size: 24px;
+      color: var(--secondary-color);
+      font-family: "Noto Sans", sans-serif;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    flex-direction: row;
+  }
+`;
+
+export const SectionTestimony = styled.section`
+  min-width: 320px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--primary-color);
+`;
