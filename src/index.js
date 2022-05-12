3a3904b92c9a7bbeb73e0b442368ce7adf031e5b
icarobteles <eng.icaroteles@gmail.com>
2022-05-12 11:24:35 -0300

    feat(index): :sparkles: import global style

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import Provider from './providers';
-import {BrowserRouter} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Provider from "./providers";
+import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "./styles/global";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider>
       <BrowserRouter>
         <App />
+        <GlobalStyle />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
-)
+);
