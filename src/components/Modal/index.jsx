21d0f210a6881d27956a85dcd63ad19945e40ef5
Kevin <kevinaryel.dev@gmail.com>
2022-05-13 10:11:03 -0300

    feat:(Modal):Added backdrop and props top/left

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,7 +1,13 @@
 import { ModalStyled } from "./style";
-const Modal = ({ children, setState, height, width }) => {
+const Modal = ({ children, setState, height, width, top, left }) => {
   return (
-    <ModalStyled setState={setState} height={height} width={width}>
+    <ModalStyled
+      setState={setState}
+      height={height}
+      width={width}
+      top={top}
+      left={left}
+    >
       <div>{children}</div>
     </ModalStyled>
   );
--- src/components/Modal/style.js
+++ src/components/Modal/style.js
@@ -1,11 +1,19 @@
 import styled from "styled-components";
 
 export const ModalStyled = styled.div`
+  z-index: auto;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.5);
   position: absolute;
   min-height: 100vh;
   min-width: 100vw;
   display: ${(props) => (props.setState ? "block" : "none")};
+
   div {
+    top: ${(props) => (props.top ? props.top : "0")};
+    left: ${(props) => (props.left ? props.left : "0")};
+    position: absolute;
     box-sizing: content-box;
     height: ${(props) => (props.height ? props.height : "auto")};
     width: ${(props) => (props.width ? props.width : "auto")};
