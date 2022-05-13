11b8feec8657f2e5e88354ea818efcc8a3d10ecb
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-05-13 10:55:08 -0300

    feat(card): Component base completed.

--- src/components/RequestCard/index.jsx
+++ src/components/RequestCard/index.jsx
@@ -1,66 +1,63 @@
-import { Header, RequestCardContainer, TableNumber } from "./styled"
+import { Header, MainContent, RequestCardContainer, TableNumber, RequestBody, FooterContent, StatusDiv } from "./styled"
 
-const ResquestCard = ({ date, table, demand }) => {
+import { MdDone } from "react-icons/md"
+import { ImCross } from "react-icons/im"
 
+const ResquestCard = ({ demand }) => {
 
-    /* demand = [
-        {
-            name: "Batata Frita",
-            description: "Batatas cortadas em palito, fritas e crocantes.",
-            rating: 3,
-            waitingTime: 13,
-            nutritionalValue: {
-                reference: "Porção de 100g - 1 unidade",
-                calorie: "101 kcal = 426 kJ",
-                carbohydrates: "7,7g",
-                proteins: "4,3g",
-                totalFats: "6,0g",
-                saturatedFat: "0,5g",
-                transFat: "0g",
-                dietaryFiber: "7,0g",
-                Sodium: "67mg"
-            },
-            additional: [{
-                name: "Queijo Cheddar",
-                sumPrice: 2
-            },
-            {
-                name: "Molho Tártaro",
-                sumPrice: 2
-            }],
-            product: [{
-                name: "Porção de batata frita pequena (1 pessoa)",
-                price: 8
-            },
-            {
-                name: "Porção de batata frita média (2 pessoas)",
-                price: 12
-            },
-            {
-                name: "Porção de batata frita grande (4 pessoas)",
-                price: 16
-            }]
-        },
-
-    ] */
-
+    const { table, date, totalPrice, totalQuantity, status, requests } = demand
 
     return (
         <RequestCardContainer>
             <Header>
                 <div>
                     <span>#345</span>
-                    <span>Data/Horário</span>
+                    <span>{date}</span>
                 </div>
-                <TableNumber>1</TableNumber>
-                <div>
-                    {demand.map((request) => {
-                        return <div>
-
-                        </div>
-                    })}
-                </div>
+                <TableNumber>{table}</TableNumber>
             </Header>
+            {requests.map((request, index) => {
+
+                const { name, image, quantity } = request
+
+                return <MainContent key={index}>
+                    <RequestBody>
+                        <img src={image} alt={name} />
+                        <div>
+                            <strong>{name}</strong>
+                            <span>Qtd.: {quantity}</span>
+                        </div>
+                    </RequestBody>
+                </MainContent>
+            })}
+            <FooterContent>
+                <div>
+                    <span>Qtd.: {totalQuantity}</span>
+                    <span>R$ {totalPrice}</span>
+                </div>
+                <div>
+                    {status === "waiting" ? (
+                        <>
+                            <StatusDiv small>
+                                <ImCross />
+                            </StatusDiv>
+                            <StatusDiv accepted small>
+                                <MdDone />
+                            </StatusDiv>
+                        </>
+                    ) : status === "accepted" ? (
+                            <StatusDiv accepted>
+                                <MdDone />
+                                <span>Aceito</span>
+                            </StatusDiv>
+                    ) : (
+                            <StatusDiv >
+                                <ImCross />
+                                <span>Rejeitado</span>
+                            </StatusDiv>
+                    )}
+                </div>
+            </FooterContent>
 
         </RequestCardContainer>
     )
