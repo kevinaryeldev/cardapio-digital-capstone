bea91e0bfe2656de4ecd15eb3ef26cb10069967a
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-05-13 11:00:04 -0300

    chore(comments): How to use RequestCard.

--- src/components/RequestCard/index.jsx
+++ src/components/RequestCard/index.jsx
@@ -7,6 +7,73 @@ const ResquestCard = ({ demand }) => {
 
     const { table, date, totalPrice, totalQuantity, status, requests } = demand
 
+
+    /* ------------------- COMO IMPORTAR O REQUESTCARD NA DASHBOARD!  ------------------- */
+
+    /* 
+    
+    Como os pedidos devem chegar no componente:
+
+    const demand1 = {
+        table: 3,
+        date: "13/05 - 12:23",
+        totalPrice: 14.00,
+        totalQuantity: 1,
+        status: "accepted",
+        requests : [{
+            name: "Porção de batata frita média (2 pessoas)",
+            aditionals: "Queijo Cheddar",
+            image: "https://img.cybercook.com.br/receitas/388/batata-frita-na-air-fryer.jpeg",
+            quantity: 1,
+        }]
+    }
+
+  const demand2 = {
+    table: 4,
+    date: "13/05 - 12:44",
+    totalPrice: 64.00,
+    totalQuantity: 4,
+    status: "waiting",
+    requests: [
+      {
+        name: "Porção de batata frita pequena (1 pessoa)",
+        image: "https://img.cybercook.com.br/receitas/388/batata-frita-na-air-fryer.jpeg",
+        quantity: 1,
+      },
+      {
+        name: "Onion Rings grande (4 pessoas)",
+        image: "https://www.anamariabrogui.com.br/assets/uploads/receitas/fotos/usuario-2528-e3730288491dc14eb7f0e3e01673350e.jpg",
+        quantity: 1,
+      },
+      {
+        name: "Filé de Saint Peter",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtkfMN2wX2YJyfIDscbu4rsj8RZ-9ns45mHw&usqp=CAU",
+        quantity: 2,
+      }
+    ]
+  }
+
+  const demand3 = {
+    table: 3,
+    date: "13/05 - 12:23",
+    totalPrice: 14.00,
+    totalQuantity: 1,
+    status: "rejected",
+    requests : [{
+      name: "Porção de batata frita média (2 pessoas)",
+      aditionals: "Queijo Cheddar",
+      image: "https://img.cybercook.com.br/receitas/388/batata-frita-na-air-fryer.jpeg",
+      quantity: 1,
+    }]
+  }
+
+    Na Dashboard:
+
+    <ResquestCard demand={demand1} />
+    <ResquestCard demand={demand2} />
+    <ResquestCard demand={demand3} />
+    */
+
     return (
         <RequestCardContainer>
             <Header>
@@ -46,15 +113,15 @@ const ResquestCard = ({ demand }) => {
                             </StatusDiv>
                         </>
                     ) : status === "accepted" ? (
-                            <StatusDiv accepted>
-                                <MdDone />
-                                <span>Aceito</span>
-                            </StatusDiv>
+                        <StatusDiv accepted>
+                            <MdDone />
+                            <span>Aceito</span>
+                        </StatusDiv>
                     ) : (
-                            <StatusDiv >
-                                <ImCross />
-                                <span>Rejeitado</span>
-                            </StatusDiv>
+                        <StatusDiv >
+                            <ImCross />
+                            <span>Rejeitado</span>
+                        </StatusDiv>
                     )}
                 </div>
             </FooterContent>
