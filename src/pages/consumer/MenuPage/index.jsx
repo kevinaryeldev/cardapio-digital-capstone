54fa6174658282cdf373f82ed35a62402fa362f0
Claudia Dias <claudiamld@outlook.com>
2022-05-14 15:17:12 -0300

    feat(menupage): create renderProducts function

--- src/pages/consumer/MenuPage/index.jsx
+++ src/pages/consumer/MenuPage/index.jsx
@@ -1,6 +1,30 @@
-import { Container } from "./style"
+import { useEffect, useState } from 'react'
+import {getProducts} from '../../../services/consumer/consumer'
+import ProductCard from '../../../components/ProductCard'
+import { Container, Content } from "./style"
 
 const MenuPage = () => {
+
+    const [products, setProducts] = useState()
+
+    const renderProducts = (value) => {
+      return (
+        value.map(((product) => {
+            return <ProductCard product={product} />    
+          }
+        ))
+      )
+    }
+
+    useEffect(() => {
+        const loadProducts = async() => {
+            const response = await getProducts()
+            setProducts(response)
+        }
+        loadProducts()
+        return;
+    }, [])
+
     return(
         <Container>
             <header>
@@ -9,6 +33,12 @@ const MenuPage = () => {
                 <a>Sobremesas</a>
                 <a>Bebidas</a>
             </header>
+            <div className="foodsection">
+                ENTRADAS
+            </div>
+            <Content>
+              {!!products && renderProducts(products)}
+            </Content>
         </Container>
     )
 }
