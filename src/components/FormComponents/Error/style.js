ed227c399fca1c083a14cf33ca0224d307e4e57e
icarobteles <eng.icaroteles@gmail.com>
2022-05-16 14:21:34 -0300

    feat(components): :sparkles: adds some properties to style the Error component

--- src/components/FormComponents/Error/style.js
+++ src/components/FormComponents/Error/style.js
@@ -4,12 +4,12 @@ import { Shake } from "../../../styles/animations";
 export const ErrorStyled = styled.span`
   animation: ${Shake} 300ms;
   width: 100%;
-  max-width: 400px;
+  max-width: ${(props) => props.maxWidth};
   text-align: left;
   font-size: 0.8rem;
   font-weight: 300;
   color: var(--error-color);
 
-  margin-top: -15px;
+  margin-top: ${(props) => props.marginTop || "-15px"};
   margin-bottom: 15px;
 `;
