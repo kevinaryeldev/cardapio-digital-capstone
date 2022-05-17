7a4c67ba70d27e9ae82c95e2ab0f599c9cb8130f
Claudia Dias <claudiamld@outlook.com>
2022-05-17 07:00:19 -0300

    feat(menupage): working on products modal

--- src/pages/consumer/MenuPage/index.jsx
+++ src/pages/consumer/MenuPage/index.jsx
@@ -35,17 +35,41 @@ const MenuPage = () => {
 
     return(
         <Container>
-          <Modal flex width={"45rem"} height={"30rem"} state={shouldOpenProductModal}>
+          <Modal 
+            flex 
+            width={"45rem"} 
+            height={"30rem"} 
+            state={shouldOpenProductModal}
+            align="center"
+            justify="center"
+            padding="15px"
+          >
             <div className='product-modal'>
               <div className='product-header'>
                 <div>
-
+                  {/* <img src={product.img} alt="product-image" /> */}
+                  <div>imagem</div>
+                  <div>estrelas</div>
                 </div>
-                <div></div>
+                <div>
+                  <h1>Batata frita</h1>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis dolor praesentium asperiores dolorem delectus recusandae. Eveniet dolor nostrum, illum velit enim corrupti ut repudiandae, odit quae nihil quos dignissimos error?</p>
+                  <span>Ver valor nutricional</span>
+                </div>
               </div>
               <div className='product-body'>
-                <div></div>
-                <div></div>
+                <div>
+                  <h2>Adicionais</h2>
+                  <div>Queijo chedar</div>
+                  <div>Molho tártaro</div>
+                </div>
+                <div>
+                  <h2>Porções</h2>
+                  <div>Porção de batata frita pequena</div>
+                  <div>Porção de batata frita pequena</div>
+                  <div>Porção de batata frita pequena</div>
+                  <button>Adicionar ao Pedido</button>
+                </div>
               </div>
             </div>
           </Modal>
