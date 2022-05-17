13e6021626733b26497a03881de61f487c1ecccc
Claudia Dias <claudiamld@outlook.com>
2022-05-17 10:47:49 -0300

    refact(dashboard): conflicts solved, page refacted

--- src/pages/collaborator/admin/DashboardPage/index.jsx
+++ src/pages/collaborator/admin/DashboardPage/index.jsx
@@ -1,6 +1,11 @@
-import Menu from "./../../../../components/Menu";
-import { FaAngleLeft, FaAngleRight, FaSearch } from "react-icons/fa";
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { FaAngleLeft, FaAngleRight, FaSearch } from "react-icons/fa";
+import Menu from "./../../../../components/Menu";
+import ProductCard from "../../../../components/ProductCard";
+import RegisterProduct from "../../../../components/RegisterProduct";
+import { useAuth } from "../../../../providers/user/user";
+import { useProducts } from "../../../../providers/products/products";
 import {
   Dashboard,
   DashboardNav,
@@ -9,19 +14,14 @@ import {
   DashboardProductsContainer,
   DashboardNavContainer,
 } from "./style";
-import ProductCard from "../../../../components/ProductCard";
-import RegisterProduct from "../../../../components/RegisterProduct";
-import { useMenu } from "../../../../providers/menu/menu";
-import { useAuth } from "../../../../providers/user/user";
-import { useHistory } from "react-router-dom";
-import { useProducts } from "../../../../providers/products/products";
   
 const DashboardPage = () => {
     const { products } = useProducts();
-    const { productsMenu } = useMenu();
-    // const inputSearch = useRef();
 
-  const [showProducts, setShowProducts] = useState(products);
+    const [openEditProduct, setOpenEditProduct] = useState(false);
+    const [openRegisterProduct, setOpenRegisterProduct] = useState(false);
+    const [productToBeEdited, setProductToBeEdited] = useState({});
+    const [showProducts, setShowProducts] = useState(products);
 
   const filter = (valueFilter) => {
     const formatedValue = valueFilter.trim().toUpperCase()
@@ -36,70 +36,61 @@ const DashboardPage = () => {
     history.push("/login");
   }
 
-  const [openRegisterProduct, setOpenRegisterProduct] = useState(false);
-  const [openEditProduct, setOpenEditProduct] = useState(false);
-  const [productToBeEdited, setProductToBeEdited] = useState({});
-
   return (
     <DashboardContainer>
       <Menu />
       <Dashboard>
-        {/* <DashboardNavContainer>
-          <DashboardNav>
-            <button>
-              <FaAngleLeft size="30px" color="white" />
-            </button>
-            <form action="">
-              <FaSearch size="30px" />
-              <input
-                type="text"
-                placeholder="Digite sua pesquisa aqui..."
-                onChange={(e) => filter(e.target.value)}
-              />
-            </form>
-            <button>
-              <FaAngleRight size="30px" color="white" />
-            </button>
-          </DashboardHeader>
-          <DashboardProductsContainer>
-            {products.length > 0 &&
-              products.map((el) => (
-                <ProductCard
-                  setOpenEditProduct={setOpenEditProduct}
-                  setProductToBeEdited={setProductToBeEdited}
-                  key={"product" + el.id}
-                  product={el}
-                />
-              ))}
-          </DashboardProductsContainer>
-        </Dashboard>
-        {openRegisterProduct && (
-          <RegisterProduct
-            type="register"
-            openModal={openRegisterProduct}
-            setOpenModal={setOpenRegisterProduct}
-          />
-        )}
-        {openEditProduct && (
-          <RegisterProduct
-            type="edit"
-            openModal={openEditProduct}
-            setOpenModal={setOpenEditProduct}
-            productToBeEdited={productToBeEdited}
-          />
-        )}
-      </DashboardContainer>
-    </>
-          </DashboardNav>
+        <DashboardNavContainer>
+        <DashboardNav>
+          <button>
+            <FaAngleLeft size="30px" color="white" />
+          </button>
+          <form action="">
+            <FaSearch size="30px" />
+            <input
+              type="text"
+              placeholder="Digite sua pesquisa aqui..."
+              onChange={(e) => filter(e.target.value)}
+            />
+          </form>
+          <button>
+            <FaAngleRight size="30px" color="white" />
+          </button>
+        </DashboardNav>
         </DashboardNavContainer>
         <DashboardHeader>
           <h2>ENTRADAS</h2>
-          <button>Adicionar</button>
+          <button onClick={() => setOpenRegisterProduct(true)}>
+            Adicionar
+          </button>
         </DashboardHeader>
         <DashboardProductsContainer>
-          {showProducts.length > 0 ? showProducts.map((item) => <ProductCard key={item.id} product={item} />) : <aside>Nenhuma produto encontrado</aside>}
-        </DashboardProductsContainer> */}
+          {!!showProducts &&
+            showProducts.map((el) => (
+              <ProductCard
+                setOpenEditProduct={setOpenEditProduct}
+                setProductToBeEdited={setProductToBeEdited}
+                key={"product" + el.id}
+                product={el}
+              />
+          ))}
+        </DashboardProductsContainer>
       </Dashboard>
+      {!!openRegisterProduct && (
+        <RegisterProduct
+          type="register"
+          openModal={openRegisterProduct}
+          setOpenModal={setOpenRegisterProduct}
+        />
+      )}
+      {!!openEditProduct && (
+        <RegisterProduct
+          type="edit"
+          openModal={openEditProduct}
+          setOpenModal={setOpenEditProduct}
+          productToBeEdited={productToBeEdited}
+        />
+      )}
     </DashboardContainer>
   );
 };
