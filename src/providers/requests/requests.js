5774e46559b484b04502f1081d2e3f28397b77b9
icarobteles <eng.icaroteles@gmail.com>
2022-05-19 13:40:13 -0300

    refactor: :recycle: exchange productsInCart for demmand

--- src/providers/requests/requests.js
+++ src/providers/requests/requests.js
@@ -23,10 +23,10 @@ export const RequestsProvider = ({ children }) => {
     setRequests(data);
   };
 
-  const sendRequestData = async (cartProducts) => {
-    const response = await addRequestApi(cartProducts);
+  const sendRequestData = async (demmand) => {
+    const response = await addRequestApi(demmand);
     if (response) {
-      getRequestData();
+      await getRequestData();
       return true;
     } else {
       return false;
