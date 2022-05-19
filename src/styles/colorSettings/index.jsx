ff5fe6cab2497eb485be1ba13b8129b49e2bc712
icarobteles <eng.icaroteles@gmail.com>
2022-05-19 10:52:14 -0300

    refactor(color settings): :fire: remove console.log

--- src/styles/colorSettings/index.jsx
+++ src/styles/colorSettings/index.jsx
@@ -20,7 +20,6 @@ const ColorSettings = ({ children }) => {
 
 
     const { primaryColor, primaryColor50, secondaryColor, secondaryColor50, terciaryColor, terciaryColor50 } = colorsSelect()
-    console.log(colorsSelect())
 
     return (
         <ColorsTheme primaryColor={primaryColor} primaryColor50={primaryColor50} secondaryColor={secondaryColor} secondaryColor50={secondaryColor50} terciaryColor={terciaryColor} terciaryColor50={terciaryColor50}>
