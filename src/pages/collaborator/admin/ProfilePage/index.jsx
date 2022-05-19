76ffb3893936107d90ad51c74c8d56840dc0ff90
icarobteles <eng.icaroteles@gmail.com>
2022-05-19 13:37:50 -0300

    refactor: :recycle: exchange history for redirect

--- src/pages/collaborator/admin/ProfilePage/index.jsx
+++ src/pages/collaborator/admin/ProfilePage/index.jsx
@@ -1,4 +1,3 @@
-import { useHistory } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -26,11 +25,12 @@ import {
   SelectColor,
   ThemeArea,
 } from "./style";
+import { Redirect } from "react-router-dom";
+import { getUserData } from "../../../../services/users/users";
 
 let { Upload } = require("upload-js");
 
 const ProfilePage = () => {
-  let history = useHistory();
   const { token, userInfos, colorTheme, setColorTheme, setColorChange, changeUserInfos } = useAuth();
   const { email, name, logoUrl, theme, categories } = userInfos;
 
@@ -461,7 +461,7 @@ const ProfilePage = () => {
   };
 
   if (!token) {
-    history.push("/");
+    return <Redirect to="/"/>
   }
 
   return (
@@ -544,7 +544,7 @@ const ProfilePage = () => {
             <ThemeArea onSubmit={handleSubmit3(setUserColors)}>
               <SelectColor>
                 <input type="color"
-                  value={inputsChange.themes?.primary.value}
+                  value={inputsChange.themes?.primary.value || "#21262D"}
                   {...register3("primaryColor")}
                   onChange={(event) =>
                     setInputsChange((prevState) => {
@@ -566,7 +566,7 @@ const ProfilePage = () => {
               </SelectColor>
               <SelectColor mid>
                 <input type="color"
-                  value={inputsChange.themes?.secondary.value}
+                  value={inputsChange.themes?.secondary.value || "#DDBC8B"}
                   {...register3("secondaryColor")}
                   onChange={(event) =>
                     setInputsChange((prevState) => {
@@ -588,7 +588,7 @@ const ProfilePage = () => {
               </SelectColor>
               <SelectColor>
                 <input type="color"
-                  value={inputsChange.themes?.terciary.value}
+                  value={inputsChange.themes?.terciary.value || "#FFFFFF"}
                   {...register3("terciaryColor")}
                   onChange={(event) =>
                     setInputsChange((prevState) => {
@@ -634,7 +634,7 @@ const ProfilePage = () => {
               {!inputsChange.category.editable && (
                 <>
                   <select
-                    defaultValue={inputsChange.category.value}
+                    defaultValue={inputsChange.category.value || "empty"}
                     name="categories"
                     id="categories"
                     onChange={(event) => {
@@ -650,9 +650,9 @@ const ProfilePage = () => {
                       console.log(inputsChange.category.value);
                     }}
                   >
-                    <option value="empty"></option>
+                    <option key="empty" value="empty"></option>
                     {categories?.map((category) => (
-                      <option value={category}>{category}</option>
+                      <option key={category} value={category}>{category}</option>
                     ))}
                   </select>
                   <button
